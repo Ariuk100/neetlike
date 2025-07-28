1cd8929ed71f3e223aa73ba77b850c5fb1be458f
Ariunbold <67946005+Ariuk100@users.noreply.github.com>
2025-07-28 21:41:58 +0800

    тодорхой тайлбартай commit message

--- lib/uploadFileToR2.ts
+++ lib/uploadFileToR2.ts
@@ -1,28 +1,24 @@
-// lib/uploadFileToR2.ts
 import { toast } from 'sonner';
-
-const NEXT_PUBLIC_BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || 'https://www.physx.mn'
-
 export async function uploadFileToR2(file: File): Promise<string> {
   try {
     const formData = new FormData();
     formData.append('file', file);
 
-    const res = await fetch(`${NEXT_PUBLIC_BASE_URL}/api/upload-to-r2`, {
+    const res = await fetch('/api/upload-to-r2', {
       method: 'POST',
       body: formData,
     });
 
     if (!res.ok) {
-      const errorData = await res.json();
-      throw new Error(errorData?.error || 'Файл байршуулахад алдаа гарлаа.');
+      const errorText = await res.text();
+      throw new Error(errorText || 'Файл байршуулахад алдаа гарлаа.');
     }
 
     const data = await res.json();
     return data.key;
-     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  } catch (error: any) {
-    toast.error(`Файл байршуулахад алдаа гарлаа: ${error.message}`);
+  } catch (error: unknown) {
+    const message = error instanceof Error ? error.message : 'Тодорхойгүй алдаа';
+    toast.error(`Файл байршуулахад алдаа гарлаа: ${message}`);
     throw error;
   }
 }
